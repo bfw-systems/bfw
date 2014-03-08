e98ea0c7f166f623948a5b84775cb702752b079f
bulton-gtw <maxou.9@gmail.com>
2014-03-09 00:07:42 +0100

    MAJ interface de la classe module

--- src/interfaces/IModules.php
+++ src/interfaces/IModules.php
@@ -15,17 +15,75 @@ interface IModules
 {
     /**
      * Permet de déclarer un nouveau modules
-     * @param string $name : Le nom du modules
-     * @param string $time [opt] : Si indiqué, le module sera chargé à un moment précis du noyau, 
-     *                              sinon il sera chargé directement.
+     * @param string $name  : Le nom du modules
+     * @param array $params : Options pour le chargement des modules.
+     *                          Liste des clés du tableau : 
+     *                              - time (string, constante) : Le moment auquel sera chargé le module. Plusieurs valeurs possible. Ce sont des constantes
+     *                                          modulesLoadTime_Module : Chargement immédiat. Avant la classe visiteur et les path en constante
+     *                                          modulesLoadTime_Visiteur : Après la classe Visiteur. Les path en constante n'existe pas
+     *                                          modulesLoadTime_EndInit : A la fin de l'initialisation du framework (défaut)
+     *                              - require (string, array) : Si le module doit avoir d'autre module de chargé avant.
      */ 
-    public function new_mods($name, $time=null);
+    public function newMod($name, $params=array());
     
     /**
      * Permet de vérifier si un module existe
+     * 
      * @param string $name : Le nom du module
+     * 
      * @return bool : true s'il existe, false sinon
      */
-    public function isset_mods($name);
+    public function exists($name);
+    
+    /**
+     * Permet de vérifier si un module est chargé
+     * 
+     * @param string $name : Le nom du module
+     * 
+     * @return bool : true s'il est chargé, false sinon
+     */
+    public function isLoad($name);
+    
+    /**
+     * Ajoute le path pour un module donné
+     * 
+     * @param string $name : Le nom du module
+     * @param string $path : Le chemin réel du module
+     */
+    public function addPath($name, $path);
+    
+    /**
+     * Liste des modules à charger à un moment précis.
+     * 
+     * @param string $timeToLoad : Le temps auquel doivent être chargé les modules
+     * 
+     * @return array : Liste des modules à charger
+     */
+    public function listToLoad($timeToLoad);
+    
+    /**
+     * Liste les modules non chargé
+     * 
+     * @param bool $regen : Permet de regénérer la liste ou non
+     * 
+     * @return array : Liste des modules non chargé
+     */
+    public function listNotLoad($regen=false);
+    
+    /**
+     * Permet de savoir si des modules n'ont pas pu être chargé
+     * 
+     * @return bool : True si des modules n'ont pas pu être chargé, false sinon.
+     */
+    public function isModulesNotLoad();
+    
+    /**
+     * Retourne les infos sur un module
+     * 
+     * @param string $name : Le nom du module dont on veux les infos
+     * 
+     * @return array : Les infos sur le module
+     */
+    public function getModuleInfos($name);
 }
 ?>
