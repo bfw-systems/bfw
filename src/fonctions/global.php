163215557d545c73153857f334fa9ede1afd6d52
bulton-fr <bulton.fr@gmail.com>
2014-03-30 16:58:09 +0200

    Add function is_session_started()

--- src/fonctions/global.php
+++ src/fonctions/global.php
@@ -278,3 +278,35 @@ function getKernel()
     
     return $BFWKernel;
 }
+
+/**
+ * Détermine si la session est démarré
+ * 
+ * @link http://fr2.php.net/manual/fr/function.session-status.php#113468
+ * 
+ * @return bool
+ */
+function is_session_started()
+{
+    $isStarted = false;
+    
+    if(php_sapi_name() !== 'cli')
+    {
+        if(version_compare(phpversion(), '5.4.0', '>='))
+        {
+            if(session_status() === PHP_SESSION_ACTIVE)
+            {
+                $isStarted = true;
+            }
+        }
+        else
+        {
+            if(session_id() !== '')
+            {
+                $isStarted = true;
+            }
+        }
+    }
+    
+    return $isStarted;
+}
