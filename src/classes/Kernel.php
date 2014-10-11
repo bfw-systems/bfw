41e25a75fe2b7e13ff9375d9b6b5af8e94a89227
bulton-fr <bulton.fr@gmail.com>
2014-10-11 20:22:38 +0200

    resolved #16

--- src/classes/Kernel.php
+++ src/classes/Kernel.php
@@ -129,41 +129,6 @@ class Kernel implements \SplSubject
     //******* Observateurs *******
     
     /**
-     * Méthode magique __call : Gère les getter et setter
-     * Est appelé dès qu'une méthode non déclaré est appelé.
-     * 
-     * @param string $name Le nom de la méthode appelé
-     * @param array  $arg  Les arguments passé à la méthode
-     * 
-     * @throws \Exception L'attribut ou la méthode demandé n'existe pas.
-     * 
-     * @return mixed Les retours prévu. 
-     */
-    public function __call($name, $arg)
-    {
-        $type = substr($name, 0, 3);
-        
-        if($type == 'get' || $type == 'set')
-        {
-            $attr = strtolower(substr($name, 4, strlen($name)));
-            if(property_exists(get_class($this), $attr))
-            {
-                if($type == 'get')
-                {
-                    return $this->{$attr};
-                }
-                elseif($type == 'set')
-                {
-                    $this->{$attr} = $arg[0];
-                    return true;
-                }
-            }
-            else {throw new \Exception('L\'attribut '.$attr.' n\'existe pas', 50);}
-        }
-        else {throw new \Exception('La méthode '.$name.' n\'existe pas.', 50);}
-    }
-    
-    /**
      * Set de l'attribut debug. Gestion de l'affichage des erreurs en plus.
      * 
      * @param bool $debug True si on est en mode débug, False sinon.
@@ -182,6 +147,16 @@ class Kernel implements \SplSubject
         {
             error_reporting(0);
         }
+    }
+    
+    /**
+     * Accesseur vers l'attribut debug
+     * 
+     * @return bool
+     */
+    public function getDebug()
+    {
+        return $this->debug;
     }
 }
 ?>
