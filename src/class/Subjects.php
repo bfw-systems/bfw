55716fc3bd2b073d004a12ff3ef4aad08fb2cd10
bulton-fr <bulton.fr@gmail.com>
2016-07-10 22:17:37 +0200

    Add Subjects class
    Implement SplSubject
    (the /classes/Observer class is deleted for the moment)

---
+++ src/class/Subjects.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace BFW;
+
+use \SplSubject;
+
+class Subjects implements SplSubject
+{
+    protected $observers = [];
+    
+    public function attach(SplObserver $observer)
+    {
+        $this->observers[] = $observer;
+        
+        return $this;
+    }
+    
+    public function detach(SplObserver $observer)
+    {
+        $key = array_search($observer, $this->observers, true);
+        
+        if($key !== false) {
+            unset($this->observers[$key]);
+        }
+        
+        return $this;
+    }
+    
+    public function notify($action = '')
+    {
+        foreach($this->observers as $observer) {
+            $observer->update($this, $action);
+        }
+        
+        return $this;
+    }
+}
