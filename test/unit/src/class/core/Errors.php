f7dde3eeabf39300764f1c73d71ed8accc6feaeb
bulton-fr <bulton.fr@gmail.com>
2016-07-31 23:31:27 +0200

    Add unit test for class core Errors
    I not write uint test for this class. It's too complicated to write...

---
+++ test/unit/src/class/core/Errors.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace BFW\Core\test\unit;
+use \atoum;
+
+require_once(__DIR__.'/../../../../../vendor/autoload.php');
+
+class Errors extends atoum
+{
+    /**
+     * @var $class : Instance de la class
+     */
+    protected $class;
+
+    /**
+     * Instanciation de la class avant chaque méthode de test
+     */
+    public function beforeTestMethod($testMethod)
+    {
+        //$this->class = new \BFW\Core\Errors;
+    }
+    
+    public function testErrors()
+    {
+        
+    }
+    
+    public function testErrorGetRender()
+    {
+        
+    }
+    
+    public function testExceptionHandler()
+    {
+        
+    }
+    
+    public function testErrorHandler()
+    {
+        
+    }
+    
+    public function testDefaultCliErrorRender()
+    {
+        
+    }
+    
+    public function testDefaultErrorRender()
+    {
+        
+    }
+}
