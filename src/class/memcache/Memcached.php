226efebfe4ea336b35e34203655ccfb5a2132fb7
bulton-fr <bulton.fr@gmail.com>
2016-08-28 23:39:08 +0200

    Update Memcached
    * call __construct : Must be call
    * get server list in the pool to check if the server is already connected or not
    * addServer not connect to the server, so add method testConnect to check if the server added is connected or not. The connection is at the first request to the server.

--- src/class/memcache/Memcached.php
+++ src/class/memcache/Memcached.php
@@ -2,6 +2,8 @@
 
 namespace BFW\Memcache;
 
+use \Exception;
+
 class Memcached extends \Memcached
 {
     use \BFW\Traits\Memcache;
@@ -17,6 +19,8 @@ class Memcached extends \Memcached
         
         if(!empty($this->config['persistentId'])) {
             parent::__construct($this->config['persistentId']);
+        } else {
+            parent::__construct();
         }
 
         $this->connectToServers();
@@ -24,7 +28,9 @@ class Memcached extends \Memcached
 
     protected function connectToServers()
     {
-        $addServers = [];
+        $addServers  = [];
+        $serversList = $this->generateServerList();
+        
         foreach ($this->config['server'] as $server) {
             $host   = isset($server['host']) ? $server['host'] : null;
             $port   = isset($server['port']) ? $server['port'] : null;
@@ -35,9 +41,45 @@ class Memcached extends \Memcached
                 continue;
             }
             
+            //It should not be to readd the server
+            //(for persistent mode particulary)
+            if(in_array($host.':'.$port, $serversList)) {
+                continue;
+            }
+            
             $addServers[] = [$host, $port, $weight];
         }
 
         $this->addServers($addServers);
+        $this->testConnect();
+    }
+    
+    /**
+     * addServer not created the connection. It's created at the first call
+     * to the memcached servers.
+     */
+    protected function testConnect()
+    {
+        $stats = $this->getStats();
+        
+        foreach ($stats as $serverName => $serverStat) {
+            if ($serverStat['uptime'] < 1) {
+                throw new Exception(
+                    'Memcached server '.$serverName.' not connected'
+                );
+            }
+        }
+    }
+    
+    protected function generateServerList()
+    {
+        $serversList = $this->getServerList();
+        $servers     = [];
+        
+        foreach ($serversList as $serverInfos) {
+            $servers[] = $serverInfos['host'].':'.$serverInfos['port'];
+        }
+        
+        return $servers;
     }
 }
