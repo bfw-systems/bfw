71606bb4780eb86686d7c204f7596b321368663f
bulton-fr <bulton.fr@gmail.com>
2016-08-28 17:30:32 +0200

    Config : Add comments for all memcache(d) configs

--- install/skeleton/config.php
+++ install/skeleton/config.php
@@ -20,16 +20,80 @@ return [
         'cli'     => '\BFW\Core\Errors::defaultCliErrorRender'
     ],
     'sqlSecureMethod' => '',
+    
+    /**
+     * Memcache(d) server(s) config
+     */
     'memcached' => [
+        /**
+         * @var boolean enabled : Enable or not connection to memcache(d)
+         */
         'enabled'      => false,
+        
+        /**
+         * @var string class : Class to use for connect to server
+         * you can use other class than BFW
+         * BFW class :
+         * * Memcached : \BFW\Memcache\Memcached
+         * * Memcache : \BFW\Memcache\Memcache
+         */
         'class'        => '\BFW\Memcache\Memcached',
+        
+        /**
+         * @var string|null persistantId : Unique ID for the instance.
+         * To create an instance that persists between requests
+         * 
+         * For memcached only.
+         * 
+         * @see http://php.net/manual/en/memcached.construct.php
+         */
         'persistentId' => null,
+        
+        /**
+         * @var array server : List of memcache(d) server(s) to connect.
+         */
         'server'       => [
+            /**
+             * First server to connect.
+             * Duplicate this array to connect at others servers.
+             */
             [
+                /**
+                 * @var string host : Memcache(d) host
+                 */
                 'host'       => '',
+                
+                /**
+                 * @var int post : Memcache(d) port
+                 */
                 'port'       => 0,
+                
+                /**
+                 * @var int|null : Value in seconds which will be used for
+                 *                  connecting to the daemon.
+                 * 
+                 * For memcache only
+                 * It's recommended to stay value at null.
+                 * 
+                 * @see http://php.net/manual/fr/memcache.connect.php
+                 */
                 'timeout'    => null,
+                
+                /**
+                 * @var boolean persistent : If the connection should be persistent
+                 * 
+                 * For memcache only
+                 */
                 'persistent' => false,
+                
+                /**
+                 * @var int weight : The weight of the server relative to the
+                 *      total weight of all the servers in the pool.
+                 * 
+                 * For memcached only
+                 * 
+                 * @see http://php.net/manual/en/memcached.addserver.php
+                 */
                 'weight'     => 0
             ]
         ]
