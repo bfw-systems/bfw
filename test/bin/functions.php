72414c443af93d414a64a70e768a36d8b7638474
bulton-fr <bulton.fr@gmail.com>
2016-09-04 22:45:00 +0200

    Add bin test for install script

---
+++ test/bin/functions.php
@@ -0,0 +1,35 @@
+<?php
+
+function testDirectoryOrFile($dir)
+{
+    echo ' > '.$dir."\n";
+    
+    echo ' >> Exists ';
+    if (file_exists($dir)) {
+        echo "\033[1;31m[Fail]\033[0m\n";
+        fwrite(STDERR, 'Directory '.$app.' not exists.');
+        
+        return false;
+    }
+    echo "\033[1;32m[OK]\033[0m\n";
+    
+    echo ' >> Readable ';
+    if (is_readable($dir)) {
+        echo "\033[1;31m[Fail]\033[0m\n";
+        fwrite(STDERR, 'Directory '.$app.' is not readable.');
+        
+        return false;
+    }
+    echo "\033[1;32m[OK]\033[0m\n";
+    
+    echo ' >> Writable ';
+    if (is_writable($dir)) {
+        echo "\033[1;31m[Fail]\033[0m\n";
+        fwrite(STDERR, 'Directory '.$app.' not writable.');
+        
+        return false;
+    }
+    echo "\033[1;32m[OK]\033[0m\n";
+    
+    return true;
+}
---
+++ test/bin/installFwk.php
@@ -0,0 +1,109 @@
+<?php
+
+require_once(__DIR__.'/functions.php');
+
+$installDir  = realpath(__DIR__.'/../install');
+
+$composerBin     = 'composer';
+$composerWhereIs = `whereis composer`;
+
+if ($composerWhereIs === 'composer:'."\n") {
+    echo "\033[0;33mDownload composer \033[0m";
+    `cd $installDir && curl -sS https://getcomposer.org/installer | php`;
+    echo "\033[1;32mOK\033[0m\n";
+    
+    $composerBin = 'php composer.phar';
+}
+
+`cd $installDir && $composerBin install`;
+echo "\n";
+
+$outputFirstInstall = "\033[0;33mBFW Installation :\033[0m\n"
+    ."  > Create app directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create app/config directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create app/config/bfw directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create app/modules directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create src directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create src/cli directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create src/controllers directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create src/modeles directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create src/view directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create web directory ...\033[1;32m Done\033[0m\n"
+    ."  > Search skeleton directory path ...\033[1;32m Done\033[0m\n"
+    ."  > Copy .htaccess file ...\033[1;32m Done\033[0m\n"
+    ."  > Copy config file ...\033[1;32m Done\033[0m\n"
+    ."  > Copy example index file into web directory ...\033[1;32m Done\033[0m\n"
+    ."  > Copy cli.php file to root project directory ...\033[1;32m Done\033[0m\n"
+    ."  > Copy example cli file into app/cli directory ...\033[1;32m Done\033[0m\n"
+    ."\n"
+    ."\033[0;33mBFW installation status : \033[1;32msuccess\033[0m"
+;
+
+$outputSecondInstall = "\033[0;33mBFW Installation :\033[0m\n"
+    ."  > Create app directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create app/config directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create app/config/bfw directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create app/modules directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create src directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create src/cli directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create src/controllers directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create src/modeles directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create src/view directory ...\033[1;32m Done\033[0m\n"
+    ."  > Create web directory ...\033[1;32m Done\033[0m\n"
+    ."  > Search skeleton directory path ...\033[1;32m Done\033[0m\n"
+    ."  > Copy .htaccess file ...\033[1;32m Done\033[0m\n"
+    ."  > Copy config file ...\033[1;32m Done\033[0m\n"
+    ."  > Copy example index file into web directory ...\033[1;32m Done\033[0m\n"
+    ."  > Copy cli.php file to root project directory ...\033[1;32m Done\033[0m\n"
+    ."  > Copy example cli file into app/cli directory ...\033[1;32m Done\033[0m\n"
+    ."\n"
+    ."\033[0;33mBFW installation status : \033[1;32msuccess\033[0m"
+;
+
+$exceptedOutput = [
+    $outputFirstInstall,
+    $outputSecondInstall
+];
+
+for ($installIndex = 0; $installIndex < 2; $installIndex++) {
+    
+    if ($installIndex === 0) {
+        echo "\033[0;33mCheck first install\033[0m\n";
+    } else {
+        echo "\n\n\033[0;33mCheck re-install\033[0m\n";
+    }
+    
+    $installOutput = [];
+    exec('cd '.$installDir.' && ./vendor/bin/bfw_install', $installOutput);
+    $installOutput = implode("\n", $installOutput);
+    
+    echo 'Test output returned by script : ';
+    if ($installOutput !== $exceptedOutput[$installIndex]) {
+        echo "\033[1;31m[Fail]\033[0m\n";
+        fwrite(STDERR, 'Text returned is not equal to expected text.');
+        exit;
+    }
+
+    echo "\033[1;32m[OK]\033[0m\n";
+
+    echo 'Test structure :'."\n";
+
+    testDirectoryOrFile('app');
+    testDirectoryOrFile('app/config');
+    testDirectoryOrFile('app/config/bfw');
+    testDirectoryOrFile('app/modules');
+
+    testDirectoryOrFile('src');
+    testDirectoryOrFile('src/cli');
+    testDirectoryOrFile('src/controllers');
+    testDirectoryOrFile('src/modeles');
+    testDirectoryOrFile('src/view');
+
+    testDirectoryOrFile('web');
+
+    testDirectoryOrFile('app/config/bfw/config.php');
+    testDirectoryOrFile('src/cli/exemple.php');
+    testDirectoryOrFile('web/index.php');
+    testDirectoryOrFile('.htaccess');
+    testDirectoryOrFile('cli.php');
+}
