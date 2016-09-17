206ac7cd7e6910de29e7ff58616016251659cc15
bulton-fr <bulton.fr@gmail.com>
2016-09-17 20:11:42 +0200

    src/class/memcache files : Add comments

--- src/class/memcache/Memcache.php
+++ src/class/memcache/Memcache.php
@@ -4,16 +4,35 @@ namespace BFW\Memcache;
 
 use \Exception;
 
+/**
+ * Class to manage connection to memcache(d) server with memcache lib
+ */
 class Memcache extends \Memcache
 {
+    //Include traits to add some methods
     use \BFW\Traits\Memcache;
     
+    /**
+     * @var \BFW\Application $app Instance of BFW Application
+     */
     protected $app;
 
+    /**
+     * @var array $config Config define on bfw config file for memcache(d)
+     */
     protected $config;
 
+    /**
+     * Constructor.
+     * Connect to servers
+     * 
+     * @param \BFW\Application $app The BFW Application instance
+     * 
+     * @throws Exception If PHP Version is >= 7.x (no memcache extension)
+     */
     public function __construct(\BFW\Application $app)
     {
+        //Check php version. No memcache lib for >= 7.x
         if (PHP_VERSION_ID > 70000) {
             throw new Exception(
                 'PHP Memcache Extension not supported for PHP 7'
@@ -26,24 +45,33 @@ class Memcache extends \Memcache
         $this->connectToServers();
     }
 
+    /**
+     * Connect to memcache(d) server defined on config
+     * 
+     * @return void
+     */
     protected function connectToServers()
     {
+        //Loop on declared server(s)
         foreach ($this->config['server'] as $server) {
             $host       = isset($server['host']) ? $server['host'] : null;
             $port       = isset($server['port']) ? $server['port'] : null;
             $timeout    = isset($server['timeout']) ? $server['timeout'] : null;
             $persistent = isset($server['persistent']) ? $server['persistent'] : false;
             
-            //not check if port = (int) 0; Doc said to define to 0 for socket.
+            //Not checked if port = (int) 0; Doc said to define to 0 for socket
             if (empty($host) || $port === null) {
                 continue;
             }
             
+            //Change method used to connect if it's a persistent connection
             $methodName = 'connect';
             if ($persistent === true) {
                 $methodName = 'pconnect';
             }
             
+            //If a timeout is declared
+            //not found the default value for this parameters, so a if...
             if ($timeout !== null) {
                 $this->{$methodName}($host, $port, $timeout);
                 return;
--- src/class/memcache/Memcached.php
+++ src/class/memcache/Memcached.php
@@ -4,14 +4,31 @@ namespace BFW\Memcache;
 
 use \Exception;
 
+/**
+ * Class to manage connection to memcache(d) server with memcached lib
+ */
 class Memcached extends \Memcached
 {
+    //Include traits to add some methods
     use \BFW\Traits\Memcache;
     
+    /**
+     * @var \BFW\Application $app Instance of BFW Application
+     */
     protected $app;
 
+    /**
+     * @var array $config Config define on bfw config file for memcache(d)
+     */
     protected $config;
 
+    /**
+     * Constructor.
+     * Call parent constructor with the persistentId if declared in config
+     * Connect to servers.
+     * 
+     * @param \BFW\Application $app The BFW Application instance
+     */
     public function __construct(\BFW\Application $app)
     {
         $this->app    = $app;
@@ -26,11 +43,22 @@ class Memcached extends \Memcached
         $this->connectToServers();
     }
 
+    /**
+     * Get the list of server already connected (persistent)
+     * Loop on server declared in config.
+     * Connect to server if not already connected
+     * 
+     * @return void
+     */
     protected function connectToServers()
     {
+        //Array to have the list of server to connect
         $addServers  = [];
+        
+        //Get all server already connected (persistent)
         $serversList = $this->generateServerList();
         
+        //Loop on server declared and config and search server not connected
         foreach ($this->config['server'] as $server) {
             $host   = isset($server['host']) ? $server['host'] : null;
             $port   = isset($server['port']) ? $server['port'] : null;
@@ -41,22 +69,29 @@ class Memcached extends \Memcached
                 continue;
             }
             
-            //It should not be to readd the server
-            //(for persistent mode particulary)
+            //search if the reading server is not already connected
             if (in_array($host.':'.$port, $serversList)) {
                 continue;
             }
             
+            //If not, add the server at the list to connect
             $addServers[] = [$host, $port, $weight];
         }
 
+        //Connect server in the list
         $this->addServers($addServers);
+        
+        //Check if connect is successfull
         $this->testConnect();
     }
     
     /**
      * addServer not created the connection. It's created at the first call
      * to the memcached servers.
+     * 
+     * So, we run the connect to all server declared
+     * 
+     * @return void
      */
     protected function testConnect()
     {
@@ -75,6 +110,11 @@ class Memcached extends \Memcached
         }
     }
     
+    /**
+     * Get the list of servers we already connected
+     * 
+     * @return string[]
+     */
     protected function generateServerList()
     {
         $serversList = $this->getServerList();
