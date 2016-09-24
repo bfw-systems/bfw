5df020caaae1413b328bd5137259fbf5acc55086
bulton-fr <bulton.fr@gmail.com>
2016-09-24 15:24:56 +0200

    Helpers Http : Add unit test file
    Not testable because exit keyword.

---
+++ test/unit/src/helpers/Http.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BFW\Helpers\test\unit;
+
+use \atoum;
+
+require_once(__DIR__.'/../../../../vendor/autoload.php');
+
+class Http extends atoum
+{
+    public function testRedirect()
+    {
+        //Not testable because exit !
+    }
+}
