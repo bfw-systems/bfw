fa65d53a3509d82c1196eb547f0e45e101e49838
bulton-fr <bulton.fr@gmail.com>
2017-07-22 14:22:28 +0200

    Skel cli exemple file : Update call to Cli methods.

--- install/skeleton/cli/exemple.php
+++ install/skeleton/cli/exemple.php
@@ -5,26 +5,26 @@
 
 use \BFW\Helpers\Cli;
 
-Cli::displayMsg('CLI Exemple file', 'green');
+Cli::displayMsgNL('CLI Exemple file', 'green');
 
 //Get parameters passed to cli
 $opt = Cli::getopt('vhp', array('version::', 'help::', 'parameters::'));
 
 //version parameter
 if (isset($opt['v']) || isset($opt['version'])) {
-    Cli::displayMsg('v0.2.1');
+    Cli::displayMsgNL('v0.2.2');
 }
 
 //Display all detected parameters passed to cli
 if (isset($opt['p']) || isset($opt['parameters'])) {
-    Cli::displayMsg(print_r($opt, true));
+    Cli::displayMsgNL(print_r($opt, true));
 }
 
 //Help message
 if (isset($opt['h']) || isset($opt['help'])) {
-    Cli::displayMsg('');
-    Cli::displayMsg('Helping Informations : Parameters script');
-    Cli::displayMsg('* -v --version : Version of test script');
-    Cli::displayMsg('* -p --parameters : Display args array');
-    Cli::displayMsg('* -h --help : View this message');
+    Cli::displayMsgNL('');
+    Cli::displayMsgNL('Helping Informations : Parameters script');
+    Cli::displayMsgNL('* -v --version : Version of test script');
+    Cli::displayMsgNL('* -p --parameters : Display args array');
+    Cli::displayMsgNL('* -h --help : View this message');
 }
