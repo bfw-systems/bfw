b88bc5a6374a007007cbafa74a0849469534a2f6
bulton-fr <bulton.fr@gmail.com>
2018-04-06 15:05:49 +0200

    Helpers\Datas::checkType throw an exception if error, not return false

--- src/helpers/Datas.php
+++ src/helpers/Datas.php
@@ -2,11 +2,31 @@
 
 namespace BFW\Helpers;
 
+use \Exception;
+
 /**
  * Helpers to manage datas
  */
 class Datas
 {
+    /**
+     * @const ERR_CHECKTYPE_ARG_TYPE Exception code if the arg type passed to
+     * checkType method is not correct.
+     */
+    const ERR_CHECKTYPE_ARG_TYPE = 1204001;
+    
+    /**
+     * @const ERR_CHECKTYPE_INFOS_FORMAT Exception code if the format of the
+     * infos passed to checkType method is not correct.
+     */
+    const ERR_CHECKTYPE_INFOS_FORMAT = 1204002;
+    
+    /**
+     * @const ERR_CHECKTYPE_DATA_OR_TYPE_VALUE_FORMAT Exception code if data or
+     * type used to check the variable has not a correct value.
+     */
+    const ERR_CHECKTYPE_DATA_OR_TYPE_VALUE_FORMAT = 1204003;
+    
     /**
      * Check types of variables
      * 
@@ -18,12 +38,18 @@ class Datas
     public static function checkType($vars)
     {
         if (!is_array($vars)) {
-            return false;
+            throw new Exception(
+                'The argument passed to the function should be an array.',
+                self::ERR_CHECKTYPE_ARG_TYPE
+            );
         }
 
         foreach ($vars as $var) {
             if (!is_array($var)) {
-                return false;
+                throw new Exception(
+                    'The informations need for the check is not in a correct format.',
+                    self::ERR_CHECKTYPE_INFOS_FORMAT
+                );
             }
 
             if (
@@ -31,7 +57,10 @@ class Datas
                 || empty($var['type'])
                 || (isset($var['type']) && !is_string($var['type']))
             ) {
-                return false;
+                throw new Exception(
+                    'Items data or type is empty or in an bad format.',
+                    self::ERR_CHECKTYPE_DATA_OR_TYPE_VALUE_FORMAT
+                );
             }
 
             if ($var['type'] === 'int') {
