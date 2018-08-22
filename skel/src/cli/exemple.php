627dec64fb926f96840634dca2444c841ebd998b
bulton-fr <bulton.fr@gmail.com>
2018-08-22 17:04:07 +0200

    Fixies cli exemple file
    Add "f:" into short options
    Use array_key_exist instead of isset

--- skel/src/cli/exemple.php
+++ skel/src/cli/exemple.php
@@ -8,20 +8,20 @@ use \BFW\Helpers\Cli;
 Cli::displayMsgNL('CLI Exemple file', 'green');
 
 //Get parameters passed to cli
-$opt = getopt('vhp', array('version::', 'help::', 'parameters::'));
+$opt = getopt('f:vhp', array('version::', 'help::', 'parameters::'));
 
 //version parameter
-if (isset($opt['v']) || isset($opt['version'])) {
+if (array_key_exists('v', $opt) || array_key_exists('version', $opt)) {
     Cli::displayMsgNL('v0.2.2');
 }
 
 //Display all detected parameters passed to cli
-if (isset($opt['p']) || isset($opt['parameters'])) {
+if (array_key_exists('p', $opt) || array_key_exists('parameters', $opt)) {
     Cli::displayMsgNL(print_r($opt, true));
 }
 
 //Help message
-if (isset($opt['h']) || isset($opt['help'])) {
+if (array_key_exists('h', $opt) || array_key_exists('help', $opt)) {
     Cli::displayMsgNL('');
     Cli::displayMsgNL('Helping Informations : Parameters script');
     Cli::displayMsgNL('* -v --version : Version of test script');
