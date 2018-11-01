8c077949b57c9c07bd01a79dc24db63d76d9a059
bulton-fr <bulton.fr@gmail.com>
2018-11-01 07:06:25 +0100

    Helpers\Cookies : Add options Path, Domain, HttpOnly, Secure, and SameSite

--- src/Helpers/Cookies.php
+++ src/Helpers/Cookies.php
@@ -8,6 +8,42 @@ namespace BFW\Helpers;
 class Cookies
 {
     /**
+     * @var string $path The path on the server in which the cookie will
+     * be available on
+     */
+    public static $path = '/';
+    
+    /**
+     * @var string $domain The (sub)domain that the cookie is available to.
+     */
+    public static $domain = '';
+    
+    /**
+     * @var bool $httpOnly When TRUE the cookie will be made accessible only
+     * through the HTTP protocol. This means that the cookie won't be
+     * accessible by scripting languages, such as JavaScript.
+     */
+    public static $httpOnly = true;
+    
+    /**
+     * @var bool $secure Indicates that the cookie should only be transmitted
+     * over a secure HTTPS connection from the client.
+     * 
+     * Please change the value to false if your site not use HTTPS connection.
+     */
+    public static $secure = true;
+    
+    /**
+     * @var string $sameSite Same-site cookies allow servers to mitigate the
+     * risk of CSRF and information leakage attacks by asserting that a
+     * particular cookie should only be sent with requests initiated
+     * from the same registrable domain.
+     * 
+     * Value can only be "lax" or "strict".
+     */
+    public static $sameSite = 'lax';
+    
+    /**
      * Create a cookie
      * 
      * @param string $name The cookie's name
@@ -18,7 +54,27 @@ class Cookies
      */
     public static function create(string $name, $value, int $expire = 1209600)
     {
-        $expireTime = time() + $expire;
-        setcookie($name, $value, $expireTime);
+        $cookieText = 'Set-Cookie: '.$name.'='.$value;
+        
+        $expireTime = new \DateTime;
+        $expireTime->modify('+ '.$expire.' second');
+        $cookieText .= '; Expires='.$expireTime->format('D, d M Y H:i:s e');
+        
+        $cookieText .= '; Path='.self::$path;
+        $cookieText .= '; Domain='.self::$domain;
+        
+        if (self::$httpOnly === true) {
+            $cookieText .= '; HttpOnly';
+        }
+        
+        if (self::$secure === true) {
+            $cookieText .= '; Secure';
+        }
+        
+        if (!empty(self::$sameSite)) {
+            $cookieText .= '; Samesite='.self::$sameSite;
+        }
+        
+        header($cookieText);
     }
 }
--- test/unit/src/Helpers/Cookies.php
+++ test/unit/src/Helpers/Cookies.php
@@ -13,42 +13,110 @@ class Cookies extends atoum
 {
     public function testCreate()
     {
-        $this->assert('test Helpers\Cookies::create with default expire time')
-            ->given($time = time())
-            ->given($setCookieArgs = [])
-            ->if($this->function->setcookie = function(...$args) use (&$setCookieArgs) {
-                $setCookieArgs = $args;
+        $this->assert('test Helpers\Cookies::create - prepare')
+            ->given($headerArg = '')
+            ->given($this->function->header = function($arg) use (&$headerArg) {
+                $headerArg = $arg;
             })
-            ->then
-            
-            ->variable(\BFW\Helpers\Cookies::create('unit_test', 'atoum'))
-                ->isNull()
-            ->string($setCookieArgs[0])
-                ->isEqualTo('unit_test')
-            ->string($setCookieArgs[1])
-                ->isEqualTo('atoum')
-            ->integer($setCookieArgs[2])
-                ->isGreaterThanOrEqualTo($time + 1209600)
-                ->isLessThanOrEqualTo($time + 1209600 + 10) //margin 10sec
         ;
         
-        $this->assert('test Helpers\Cookies::create with an expire time')
-            ->given($time = time())
-            ->given($setCookieArgs = [])
-            ->if($this->function->setcookie = function(...$args) use (&$setCookieArgs) {
-                $setCookieArgs = $args;
-            })
+        $this->assert('test Helpers\Cookies::create with default expire time')
+            ->given($expireTime = new \DateTime)
+            ->and($expireTime->modify('+1209600 second'))
             ->then
-            
+            ->variable(\BFW\Helpers\Cookies::create('unit_test', 'atoum'))
+                ->isNull()
+            //Cannot use ->function('header')->wasCalledWithArguments(...
+            //Because the expire time have second
+            ->given($cookieArgs = explode('; ', $headerArg))
+            ->string($cookieArgs[0])
+                ->isEqualTo('Set-Cookie: unit_test=atoum')
+            ->string($cookieArgs[1])
+                //Expires=Thu, 15 Nov 2018 06:46:16 Europe/Berlin
+                ->matches('#Expires=[A-Z][a-z]{2}, [0-9]{2} [A-Z][a-z]{2} [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2} '.$expireTime->format('e').'#')
+            ->string($cookieArgs[2])
+                ->isEqualTo('Path=/')
+            ->string($cookieArgs[3])
+                ->isEqualTo('Domain=')
+            ->string($cookieArgs[4])
+                ->isEqualTo('HttpOnly')
+            ->string($cookieArgs[5])
+                ->isEqualTo('Secure')
+            ->string($cookieArgs[6])
+                ->isEqualTo('Samesite=lax')
+            ->then
+            ->given($expireMatch = [])
+            ->and(preg_match(
+                '#Expires=([A-Z][a-z]{2}, [0-9]{2} [A-Z][a-z]{2} [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}) '.$expireTime->format('e').'#',
+                $cookieArgs[1],
+                $expireMatch
+            ))
+            ->and($cookieTime = \DateTime::createFromFormat(
+                'D, d M Y H:i:s',
+                $expireMatch[1]
+            ))
+            ->given($cookieTimestamp = (int) $cookieTime->format('U'))
+            ->integer($cookieTimestamp)
+                ->isGreaterThanOrEqualTo((int) $expireTime->format('U'))
+                ->isLessThanOrEqualTo((int) $expireTime->format('U') + 10) //margin 10sec
+        ;
+        
+        $this->assert('test Helpers\Cookies::create with default expire time')
+            ->given($expireTime = new \DateTime)
+            ->and($expireTime->modify('+42 second'))
+            ->then
             ->variable(\BFW\Helpers\Cookies::create('unit_test', 'atoum', 42))
                 ->isNull()
-            ->string($setCookieArgs[0])
-                ->isEqualTo('unit_test')
-            ->string($setCookieArgs[1])
-                ->isEqualTo('atoum')
-            ->integer($setCookieArgs[2])
-                ->isGreaterThanOrEqualTo($time + 42)
-                ->isLessThanOrEqualTo($time + 42 + 10) //margin 10sec
+            //Cannot use ->function('header')->wasCalledWithArguments(...
+            //Because the expire time have second
+            ->given($cookieArgs = explode('; ', $headerArg))
+            ->array($cookieArgs)
+                ->size
+                    ->isEqualTo(7)
+            ->string($cookieArgs[0])
+                ->isEqualTo('Set-Cookie: unit_test=atoum')
+            ->string($cookieArgs[1])
+                //Expires=Thu, 15 Nov 2018 06:46:16 Europe/Berlin
+                ->matches('#Expires=[A-Z][a-z]{2}, [0-9]{2} [A-Z][a-z]{2} [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2} '.$expireTime->format('e').'#')
+            ->then
+            ->given($expireMatch = [])
+            ->and(preg_match(
+                '#Expires=([A-Z][a-z]{2}, [0-9]{2} [A-Z][a-z]{2} [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}) '.$expireTime->format('e').'#',
+                $cookieArgs[1],
+                $expireMatch
+            ))
+            ->and($cookieTime = \DateTime::createFromFormat(
+                'D, d M Y H:i:s',
+                $expireMatch[1]
+            ))
+            ->given($cookieTimestamp = (int) $cookieTime->format('U'))
+            ->integer($cookieTimestamp)
+                ->isGreaterThanOrEqualTo((int) $expireTime->format('U'))
+                ->isLessThanOrEqualTo((int) $expireTime->format('U') + 10) //margin 10sec
+        ;
+        
+        $this->assert('test Helpers\Cookies::create without httpOnly, Secure and Samesite options')
+            ->if(\BFW\Helpers\Cookies::$httpOnly = false)
+            ->and(\BFW\Helpers\Cookies::$secure = false)
+            ->and(\BFW\Helpers\Cookies::$sameSite = null)
+            ->then
+            ->variable(\BFW\Helpers\Cookies::create('unit_test', 'atoum'))
+                ->isNull()
+            //Cannot use ->function('header')->wasCalledWithArguments(...
+            //Because the expire time have second
+            ->given($cookieArgs = explode('; ', $headerArg))
+            ->array($cookieArgs)
+                ->size
+                    ->isEqualTo(4)
+            ->string($cookieArgs[0])
+                ->isEqualTo('Set-Cookie: unit_test=atoum')
+            ->string($cookieArgs[1])
+                //Expires=Thu, 15 Nov 2018 06:46:16 Europe/Berlin
+                ->matches('#Expires=[A-Z][a-z]{2}, [0-9]{2} [A-Z][a-z]{2} [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2} '.$expireTime->format('e').'#')
+            ->string($cookieArgs[2])
+                ->isEqualTo('Path=/')
+            ->string($cookieArgs[3])
+                ->isEqualTo('Domain=')
         ;
     }
 }
