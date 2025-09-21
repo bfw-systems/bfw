7fa6a57cd97d03504dc1f07a20d02bbe8d3e8efc
bulton-fr <bulton.fr@gmail.com>
2025-09-21 14:26:32 +0200

    Update atoum config file for atoum 4.x
    PHP 7.2 minimum for unit test in this PR, so composer install atoum 4.0.x

--- .atoum.php
+++ .atoum.php
@@ -1,52 +1,25 @@
 <?php
 
-/*
-This file will automatically be included before EACH run.
+require __DIR__ . '/vendor/autoload.php';
 
-Use it to configure atoum or anything that needs to be done before EACH run.
+$script->addDefaultReport();
 
-More information on documentation:
-[en] http://docs.atoum.org/en/chapter3.html#Configuration-files
-[fr] http://docs.atoum.org/fr/chapter3.html#Fichier-de-configuration
-*/
-
-use \mageekguy\atoum;
-//use \mageekguy\atoum\reports;
-
-// CODE COVERAGE SETUP
-if(!getenv('GITHUB_ACTIONS'))
-{
-    $report = $script->addDefaultReport();
+// Only enable code coverage in CI with proper Xdebug mode
+if (getenv('CI') === 'true' && getenv('ENABLE_COVERAGE') === 'true' && 
+    extension_loaded('xdebug') && ini_get('xdebug.mode') !== false) {
     
-    $coverageField = new atoum\report\fields\runner\coverage\html('BFW', __DIR__.'/test/unit/report/coverage');
-    //$coverageField->setRootUrl('http://bfw.bulton.fr/reports/bfw-v3/test-unit/index.html');
-    $report->addField($coverageField);
+    $script->noCodeCoverageForClasses('\\atoum\\atoum\\*');
+    $script->excludeDirectoriesFromCoverage([__DIR__ . '/vendor']);
     
-    $treemapField = new atoum\report\fields\runner\coverage\treemap('BFW', __DIR__.'/test/unit/report/treemap');
-    //$treemapField->setHtmlReportBaseUrl('http://bfw.bulton.fr/reports/bfw-v3/treemap/index.html');
-    $report->addField($treemapField);
+    // Add XML coverage report for Codecov using full class paths (PHP 7.x compatible)
+    $coverageReportPath = __DIR__ . '/coverage.xml';
+    $cloverReport = new \atoum\atoum\reports\asynchronous\clover();
+    $fileWriter = new \atoum\atoum\writers\file($coverageReportPath);
+    $cloverReport->addWriter($fileWriter);
+    $script->addReport($cloverReport);
+} else {
+    // Disable code coverage by default to avoid errors
+    $script->disableCodeCoverage();
 }
-/**/
 
-/*
-TEST GENERATOR SETUP
-*//*
-$script->getRunner()->addTestsFromDirectory(__DIR__.'/test/unit/install/class');
-$script->getRunner()->addTestsFromDirectory(__DIR__.'/test/unit/src/class');
-$script->getRunner()->addTestsFromDirectory(__DIR__.'/test/unit/src/class/core');
-$script->getRunner()->addTestsFromDirectory(__DIR__.'/test/unit/src/class/memcache');
-//$script->getRunner()->addTestsFromDirectory(__DIR__.'/test/unit/src/functions');
-//$script->getRunner()->addTestsFromDirectory(__DIR__.'/test/unit/src/trait');
-/**/
-
-if(getenv('GITHUB_ACTIONS'))
-{
-    $script->addDefaultReport(); //For GitHub Actions debug!
-    
-    // Generate clover coverage report for Codecov
-    $cloverWriter = new atoum\writers\file('clover.xml');
-    $cloverReport = new atoum\reports\asynchronous\clover();
-    $cloverReport->addWriter($cloverWriter);
-
-    $runner->addReport($cloverReport);
-}
+$runner->addTestsFromDirectory(__DIR__.'/test/unit/src');
